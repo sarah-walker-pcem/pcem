2adb1103dc12640e27b1d2211a8f9c1d5ea4762d
TomW <TomW@localhost>
2013-07-20 13:42:31 +0100

    SiS496/497 CMOS file now handled correctly.

--- src/nvr.c
+++ src/nvr.c
@@ -162,7 +162,8 @@ void loadnvr()
                 case ROM_AMI486:   f = romfopen("ami486.nvr",  "rb"); nvrmask = 127; break;
                 case ROM_WIN486:   f = romfopen("win486.nvr",  "rb"); nvrmask = 127; break;
                 case ROM_PCI486:   f = romfopen("hot-433.nvr", "rb"); nvrmask = 127; break;
-                case ROM_430VX:    f = romfopen("430vx.nvr", "rb"); nvrmask = 127; break;
+                case ROM_SIS496:   f = romfopen("sis496.nvr",  "rb"); nvrmask = 127; break;
+                case ROM_430VX:    f = romfopen("430vx.nvr",   "rb"); nvrmask = 127; break;
                 default: return;
         }
         if (!f)
@@ -197,8 +198,9 @@ void savenvr()
                 case ROM_AMI386:   f = romfopen("ami386.nvr" , "wb"); break;
                 case ROM_AMI486:   f = romfopen("ami486.nvr" , "wb"); break;
                 case ROM_WIN486:   f = romfopen("win486.nvr" , "wb"); break;
-                case ROM_PCI486:   f = romfopen("hot-433.nvr", "wb"); break;                
-                case ROM_430VX:    f = romfopen("430vx.nvr", "wb"); break;
+                case ROM_PCI486:   f = romfopen("hot-433.nvr", "wb"); break;
+                case ROM_SIS496:   f = romfopen("sis496.nvr" , "wb"); break;
+                case ROM_430VX:    f = romfopen("430vx.nvr"  , "wb"); break;
                 default: return;
         }
         fwrite(nvrram,128,1,f);
