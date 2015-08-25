b53c7d32f9419ca053b4cd79af76d19d8530e474
TomW <TomW@localhost>
2015-08-25 19:32:15 +0100

    Added dummy RDPMC implementation.

--- src/x86_ops_msr.h
+++ src/x86_ops_msr.h
@@ -16,3 +16,15 @@ static int opRDTSC(uint32_t fetchdat)
         CLOCK_CYCLES(1);
         return 0;
 }
+
+static int opRDPMC(uint32_t fetchdat)
+{
+        if (ECX > 1 || (!(cr4 & CR4_PCE) && (cr0 & 1) && CPL))
+        {
+                x86gpf("RDPMC not allowed", 0);
+                return 1;
+        }
+        EAX = EDX = 0;
+        CLOCK_CYCLES(1);
+        return 0;
+}
