8d90f77279d497af0598c132792964dae46afd5b
TomW <TomW@localhost>
2016-03-29 20:06:55 +0100

    Access bit now set in selector descriptors.

--- src/x86seg.c
+++ src/x86seg.c
@@ -8,16 +8,12 @@
 #include "386.h"
 #include "cpu.h"
 
-/*Controls whether the accessed bit in a descriptor is set when CS is loaded. The
-  386 PRM is ambiguous on this subject, but BOCHS doesn't set it and Windows 98
-  setup crashes if it is.
-  The accessed bit is always set for data and stack selectors though.*/
-//#define CS_ACCESSED
+/*Controls whether the accessed bit in a descriptor is set when CS is loaded.*/
+#define CS_ACCESSED
 
 /*Controls whether the accessed bit in a descriptor is set when a data or stack
-  selector is loaded. This SHOULD be set, however Windows 98 setup crashes if it
-  is.*/
-//#define SEL_ACCESSED
+  selector is loaded.*/
+#define SEL_ACCESSED
 int stimes = 0;
 int dtimes = 0;
 int btimes = 0;
