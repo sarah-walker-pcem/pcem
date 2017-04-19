f41ad3611152e16da6d7a54f141b7ac101444dbd
SarahW <SarahW@localhost>
2017-04-19 16:23:43 +0100

    Inline functions in 808x.c marked as static. Patch from AmatCoder.

--- src/808x.c
+++ src/808x.c
@@ -117,7 +117,7 @@ int fetchcycles=0,memcycs,fetchclocks;
 uint8_t prefetchqueue[6];
 uint16_t prefetchpc;
 int prefetchw=0;
-inline uint8_t FETCH()
+static inline uint8_t FETCH()
 {
         uint8_t temp;
 /*        temp=prefetchqueue[0];
@@ -171,7 +171,7 @@ inline uint8_t FETCH()
         return temp;
 }
 
-inline void FETCHADD(int c)
+static inline void FETCHADD(int c)
 {
         int d;
 //        if (output) printf("FETCHADD %i\n",c);
@@ -226,7 +226,7 @@ void FETCHCOMPLETE()
                 fetchcycles+=(4-(fetchcycles&3));
 }
 
-inline void FETCHCLEAR()
+static inline void FETCHCLEAR()
 {
 /*        int c;
         fetchcycles=0;
