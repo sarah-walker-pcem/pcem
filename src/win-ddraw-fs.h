e44ca65ad6125f30cedcda660264f9be968e253c
SarahW <pcem@pcem-emulator.co.uk>
2018-06-04 22:08:38 +0100

    Removed old bitrotted Win32 port.

--- src/win-ddraw-fs.h
+++
@@ -1,8 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-        void ddraw_fs_init(HWND h);
-        void ddraw_fs_close();
-#ifdef __cplusplus
-}
-#endif
--- src/win-ddraw.h
+++
@@ -1,9 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-        void ddraw_init(HWND h);
-        void ddraw_close();
-#ifdef __cplusplus
-}
-#endif
-
