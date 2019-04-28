57be2ef8d4bae8cb4269cb022f1522443518e9c8
SarahW <pcem@pcem-emulator.co.uk>
2019-04-28 18:35:11 +0100

    Add ability to change AT keyboard scancode and disable translation. Fixes keyboard in Solaris 8.

--- src/keyboard.h
+++ src/keyboard.h
@@ -4,3 +4,12 @@ void keyboard_process();
 extern int keyboard_scan;
 
 extern uint8_t pcem_key[272];
+
+enum
+{
+        SCANCODE_SET_1,
+        SCANCODE_SET_2,
+        SCANCODE_SET_3
+};
+
+void keyboard_set_scancode_set(int set);
--- src/keyboard_at.c
+++ src/keyboard_at.c
@@ -36,6 +36,10 @@ struct
         uint8_t mem[0x20];
         uint8_t out;
         int out_new, out_delayed;
+
+        int scancode_set;
+        int translate;
+        int next_is_release;
         
         uint8_t input_port;
         uint8_t output_port;
@@ -56,6 +60,26 @@ struct
 	pc_timer_t send_delay_timer;
 } keyboard_at;
 
+/*Translation table taken from https://www.win.tue.nl/~aeb/linux/kbd/scancodes-10.html#ss10.3*/
+static uint8_t at_translation[256] =
+{
+        0xff, 0x43, 0x41, 0x3f, 0x3d, 0x3b, 0x3c, 0x58, 0x64, 0x44, 0x42, 0x40, 0x3e, 0x0f, 0x29, 0x59,
+        0x65, 0x38, 0x2a, 0x70, 0x1d, 0x10, 0x02, 0x5a, 0x66, 0x71, 0x2c, 0x1f, 0x1e, 0x11, 0x03, 0x5b,
+        0x67, 0x2e, 0x2d, 0x20, 0x12, 0x05, 0x04, 0x5c, 0x68, 0x39, 0x2f, 0x21, 0x14, 0x13, 0x06, 0x5d,
+        0x69, 0x31, 0x30, 0x23, 0x22, 0x15, 0x07, 0x5e, 0x6a, 0x72, 0x32, 0x24, 0x16, 0x08, 0x09, 0x5f,
+        0x6b, 0x33, 0x25, 0x17, 0x18, 0x0b, 0x0a, 0x60, 0x6c, 0x34, 0x35, 0x26, 0x27, 0x19, 0x0c, 0x61,
+        0x6d, 0x73, 0x28, 0x74, 0x1a, 0x0d, 0x62, 0x6e, 0x3a, 0x36, 0x1c, 0x1b, 0x75, 0x2b, 0x63, 0x76,
+        0x55, 0x56, 0x77, 0x78, 0x79, 0x7a, 0x0e, 0x7b, 0x7c, 0x4f, 0x7d, 0x4b, 0x47, 0x7e, 0x7f, 0x6f,
+        0x52, 0x53, 0x50, 0x4c, 0x4d, 0x48, 0x01, 0x45, 0x57, 0x4e, 0x51, 0x4a, 0x37, 0x49, 0x46, 0x54,
+        0x80, 0x81, 0x82, 0x41, 0x54, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f,
+        0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d, 0x9e, 0x9f,
+        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf,
+        0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5, 0xb6, 0xb7, 0xb8, 0xb9, 0xba, 0xbb, 0xbc, 0xbd, 0xbe, 0xbf,
+        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf,
+        0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda, 0xdb, 0xdc, 0xdd, 0xde, 0xdf,
+        0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee, 0xef,
+        0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff
+};
 static uint8_t key_ctrl_queue[16];
 static int key_ctrl_queue_start = 0, key_ctrl_queue_end = 0;
 
@@ -173,6 +197,21 @@ void keyboard_at_adddata_keyboard(uint8_t val)
 			case 0x4D: t3100e_notify_set(0x0F); break; /* Right */
 		}
 	}
+	if (keyboard_at.translate)
+	{
+                if (val == 0xf0)
+                {
+                        keyboard_at.next_is_release = 1;
+                        return;
+                }
+                else
+                {
+                        val = at_translation[val];
+                        if (keyboard_at.next_is_release)
+                                val |= 0x80;
+                        keyboard_at.next_is_release = 0;
+                }
+        }
         key_queue[key_queue_end] = val;
         key_queue_end = (key_queue_end + 1) & 0xf;
 //        pclog("keyboard_at : %02X added to key queue\n", val);
@@ -221,6 +260,7 @@ void keyboard_at_write(uint16_t port, uint8_t val, void *priv)
                                         if (!(val & 1) && keyboard_at.wantirq)
                                            keyboard_at.wantirq = 0;
                                         mouse_scan = !(val & 0x20);
+                                        keyboard_at.translate = val & 0x40;
                                 }                                           
                                 break;
 
@@ -274,6 +314,45 @@ void keyboard_at_write(uint16_t port, uint8_t val, void *priv)
                                         case 0xed: /*Set/reset LEDs*/
                                         keyboard_at_adddata_keyboard(0xfa);
                                         break;
+                                        
+                                        case 0xf0: /*Set scancode set*/
+                                        switch (val)
+                                        {
+                                                case 0: /*Read current set*/
+                                                keyboard_at_adddata_keyboard(0xfa);
+                                                switch (keyboard_at.scancode_set)
+                                                {
+                                                        case SCANCODE_SET_1:
+                                                        keyboard_at_adddata_keyboard(0x01);
+                                                        break;
+                                                        case SCANCODE_SET_2:
+                                                        keyboard_at_adddata_keyboard(0x02);
+                                                        break;
+                                                        case SCANCODE_SET_3:
+                                                        keyboard_at_adddata_keyboard(0x03);
+                                                        break;
+                                                }
+                                                break;
+                                                case 1:
+                                                keyboard_at.scancode_set = SCANCODE_SET_1;
+                                                keyboard_set_scancode_set(SCANCODE_SET_1);
+                                                keyboard_at_adddata_keyboard(0xfa);
+                                                break;
+                                                case 2:
+                                                keyboard_at.scancode_set = SCANCODE_SET_2;
+                                                keyboard_set_scancode_set(SCANCODE_SET_2);
+                                                keyboard_at_adddata_keyboard(0xfa);
+                                                break;
+                                                case 3:
+                                                keyboard_at.scancode_set = SCANCODE_SET_3;
+                                                keyboard_set_scancode_set(SCANCODE_SET_3);
+                                                keyboard_at_adddata_keyboard(0xfa);
+                                                break;
+                                                default:
+                                                keyboard_at_adddata_keyboard(0xfe);
+                                                break;
+                                        }
+                                        break;
 
                                         case 0xf3: /*Set typematic rate/delay*/
                                         keyboard_at_adddata_keyboard(0xfa);
@@ -299,6 +378,11 @@ void keyboard_at_write(uint16_t port, uint8_t val, void *priv)
                                         keyboard_at_adddata_keyboard(0xfa);
                                         break;
                                         
+                                        case 0xf0: /*Set scancode set*/
+                                        keyboard_at.key_wantdata = 1;
+                                        keyboard_at_adddata_keyboard(0xfa);
+                                        break;
+
                                         case 0xf2: /*Read ID*/
                                         keyboard_at_adddata_keyboard(0xfa);
                                         keyboard_at_adddata_keyboard(0xab);
@@ -639,6 +723,7 @@ void keyboard_at_init()
         keyboard_at.mouse_write = NULL;
         keyboard_at.mouse_p = NULL;
         keyboard_at.is_ps2 = 0;
+        keyboard_set_scancode_set(SCANCODE_SET_2);
         
         timer_add(&keyboard_at.send_delay_timer, keyboard_at_poll, NULL, 1);
 }
