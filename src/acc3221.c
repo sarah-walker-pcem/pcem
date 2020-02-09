a0a6b0faa8d6aa56d6bd8b4b524a5ba83b608bc3
SarahW <pcem@pcem-emulator.co.uk>
2020-02-09 12:59:46 +0000

    Fix serial IRQs on ACC3221. Patch from EluanCM.

--- src/acc3221.c
+++ src/acc3221.c
@@ -55,7 +55,7 @@ static void acc3221_write(uint16_t addr, uint8_t val, void *p)
 //                                        pclog("COM1 addr %03x IRQ 3\n", addr);
                                         serial1_set(addr, 3);
                                 }
-                                else if ((acc3221.regs[0xde] & REG_DE_SIRQ4_SOURCE) == REG_DE_SIRQ3_SERIAL1)
+                                else if ((acc3221.regs[0xde] & REG_DE_SIRQ4_SOURCE) == REG_DE_SIRQ4_SERIAL1)
                                 {
 //                                        pclog("COM1 addr %03x IRQ 4\n", addr);
                                         serial1_set(addr, 4);
@@ -76,7 +76,7 @@ static void acc3221_write(uint16_t addr, uint8_t val, void *p)
 //                                        pclog("COM2 addr %03x IRQ 3\n", addr);
                                         serial2_set(addr, 3);
                                 }
-                                else if ((acc3221.regs[0xde] & REG_DE_SIRQ4_SOURCE) == REG_DE_SIRQ3_SERIAL2)
+                                else if ((acc3221.regs[0xde] & REG_DE_SIRQ4_SOURCE) == REG_DE_SIRQ4_SERIAL2)
                                 {
 //                                        pclog("COM2 addr %03x IRQ 4\n", addr);
                                         serial2_set(addr, 4);
