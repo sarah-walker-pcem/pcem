92e0b785480a2a5e49aac3eac97c3bab156b13ad
SarahW <pcem@pcem-emulator.co.uk>
2020-09-29 20:40:53 +0100

    Add missing svga_render_null() function.

--- src/vid_svga_render.h
+++ src/vid_svga_render.h
@@ -9,6 +9,7 @@ extern int scrollcache;
 
 extern uint8_t edatlookup[4][4];
 
+void svga_render_null(svga_t *svga);
 void svga_render_blank(svga_t *svga);
 void svga_render_text_40(svga_t *svga);
 void svga_render_text_80(svga_t *svga);
