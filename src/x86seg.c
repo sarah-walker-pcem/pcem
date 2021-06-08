b62acab9aedc0bd0b022c882f42354d3c166211b
C.W. Betts <computers57@hotmail.com>
2021-06-08 15:08:20 -0600

    Update x86seg.c
    Evaluates the bitwise operator first.
    Fixes a warning in Clang: "logical not is only applied to the left hand side of this bitwise operator [-Wlogical-not-parentheses]"

--- src/x86seg.c
+++ src/x86seg.c
@@ -3058,7 +3058,7 @@ void x86_smi_enter(void)
         {
                 uint32_t base;
 
-                if (!cyrix.smhr & SMHR_VALID)
+                if (!(cyrix.smhr & SMHR_VALID))
                         cyrix.smhr = (cyrix.arr[3].base + cyrix.arr[3].size) | SMHR_VALID;
                 base = cyrix.smhr & SMHR_ADDR_MASK;
 
