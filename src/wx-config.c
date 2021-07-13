00ddff504fdef31e3cdd779bf1354fd9ba359be4
Jakub Strzelecki <80216842+kubast2@users.noreply.github.com>
2021-07-13 11:23:41 +0200

    Add fallocate for linux target
    Needs verification if it works with clang and gcc toolchain as is.

--- src/wx-config.c
+++ src/wx-config.c
@@ -1567,6 +1567,14 @@ static int create_drive_raw(void* data)
         int c;
         uint8_t buf[512];
         FILE* f = (FILE*)data;
+
+        #ifdef __linux__
+          if(fallocate(fdopen(f), FALLOC_FL_ZERO_RANGE, 0, (hd_new_cyl * hd_new_hpc * hd_new_spt) ) == 0)) {
+            create_drive_pos = (hd_new_cyl * hd_new_hpc * hd_new_spt);
+            return 1;
+          }
+        #endif
+
         memset(buf, 0, 512);
         for (c = 0; c < (hd_new_cyl * hd_new_hpc * hd_new_spt); c++)
         {
