8db985a2c02ba79d405606e3af81483ff399ae4f
Michael J. Manley <mjmanley@ntge.net>
2021-12-17 04:13:19 -0800

    Final Fixes to allow workflows to work.

--- src/wx-main.cc
+++ src/wx-main.cc
@@ -6,6 +6,15 @@ extern "C"
 int pc_main(int, char**);
 }
 
+#ifdef WIN32
+#include <winbase.h>
+
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd)
+{
+    return main(__argc, __argv);
+}
+#endif
+
 int main(int argc, char **argv)
 {
         if (!pc_main(argc, argv))
