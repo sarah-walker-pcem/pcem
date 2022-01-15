8ee2ce59fdac833226e1529191f6ff91cdd0c35b
sarah-walker-pcem <67199234+sarah-walker-pcem@users.noreply.github.com>
2022-01-15 23:45:24 +0000

    MinG/W build fixes. (#86)

--- src/main.c
+++ src/main.c
@@ -3,8 +3,9 @@ extern int pcem_lib_main(int argc, char **argv);
 #ifdef WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <stdlib.h>
 
-int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd)
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char *pCmdLine, int nShowCmd)
 {
     return pcem_lib_main(__argc, __argv);
 }
