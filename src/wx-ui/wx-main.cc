e9840c0a3ec884aa35a293739c45d147568b49b1
Michael J. Manley <mjmanley@ntge.net>
2022-01-24 14:49:27 -0800

    This should hopefully fix #90. Also needed to build on Windows. Going to run CI on this one.

--- src/wx-ui/wx-main.cc
+++ src/wx-ui/wx-main.cc
@@ -1,14 +1,28 @@
 #include "wx-app.h"
 #include <SDL2/SDL.h>
 
+#if WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
 extern "C"
 {
 int pc_main(int, char**);
 int main(int argc, char** argv);
 }
 
+#if WIN32
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd)
+#else
 int main(int argc, char** argv)
+#endif
 {
+#if WIN32
+        int argc = __argc;
+        char** argv = __argv;
+#endif
+
         if (!pc_main(argc, argv))
                 return -1;
 
