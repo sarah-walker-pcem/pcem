3bbccb1b882b4f4a0af16b3f78fc3a5875111617
Michael J. Manley <mjmanley@ntge.net>
2022-05-11 01:11:19 -0700

    Added some helpful cmake scripts to allow cross compiling on Fedora.

---
+++ cmake/fedora-mingw.cmake
@@ -0,0 +1,18 @@
+set(CMAKE_SYSTEM_NAME Windows)
+
+# cross compilers to use for C and C++
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_PREFIX}-gfortran)
+set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+
+# target environment on the build host system
+#   set 1st to dir with the cross compiler's C/C++ headers/libs
+set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX} /usr/${TOOLCHAIN_PREFIX}/sys-root/mingw)
+
+# modify default behavior of FIND_XXX() commands to
+# search for headers/libs in the target environment and
+# search for programs in the build host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
---
+++ cmake/fedora-mingw32-cross-toolchain.cmake
@@ -0,0 +1,4 @@
+set(TOOLCHAIN_PREFIX i686-w64-mingw32)
+set(CMAKE_SYSTEM_PROCESSOR i386)
+
+include(${CMAKE_CURRENT_LIST_DIR}/fedora-mingw.cmake)
