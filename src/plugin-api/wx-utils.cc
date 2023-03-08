049d08714c7f8f35872a93bf8c7e4a0008285243
ruben-balea <98262033+ruben-balea@users.noreply.github.com>
2023-03-08 21:05:28 +0100

    Update wx-utils.cc
    Fix for Windows home directory with spaces

--- src/plugin-api/wx-utils.cc
+++ src/plugin-api/wx-utils.cc
@@ -12,6 +12,20 @@ void wx_get_home_directory(char *path) {
         if (!home.EndsWith(wxFileName::GetPathSeparator())) {
                 home.Append(wxFileName::GetPathSeparator());
         }
+        #ifdef _WIN32
+        wxString str = home;
+        int i1, i2, len = str.length();
+        for (i1 = 0; i1 < len; i1++) {
+            if (str[i1] == '\"') {
+                for (i2 = i1; i2 < len - 1; i2++) {
+                    str[i2] = str[i2+1];
+                }
+                len--;
+                i1--;
+            }
+        }        
+        home = str;
+        #endif        
         strcpy(path, home.mb_str());
 }
 
