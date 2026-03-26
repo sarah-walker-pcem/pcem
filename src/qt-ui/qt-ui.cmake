187e6bb877504af5a2655bd6ce60e1943e87d896
Josep Maria Antolín <josepma@gmail.com>
2026-03-26 22:22:09 +0100

    Fix windows packaging. The new Qt build requires a platform dll from Qt copied to the binary folder Also, the libnpcap.dll provided previously required to install npcap even if not using npcap (the application wouldn't start due to missing packet.dll). Restored to use the older libnpcap from msys2, and adding a comment to the README on how to use NPCAP on windows.

--- src/qt-ui/qt-ui.cmake
+++ src/qt-ui/qt-ui.cmake
@@ -9,10 +9,10 @@ set(PCEM_PRIVATE_API ${PCEM_PRIVATE_API}
         ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-glslp-parser.h
         ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-hostconfig.h
         ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-joystickconfig.h
-        ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-sdl2-glw.h
         ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-sdl2.h
-        ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-sdl2-video-gl3.h
+        ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-sdl2-glw.h
         ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-sdl2-video.h
+        ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-sdl2-video-gl3.h
         ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-sdl2-video-renderer.h
         ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-shaderconfig.h
         ${CMAKE_SOURCE_DIR}/includes/private/qt-ui/qt-status.h
@@ -20,30 +20,39 @@ set(PCEM_PRIVATE_API ${PCEM_PRIVATE_API}
         )
 
 set(PCEM_SRC ${PCEM_SRC}
-        qt-ui/qt-main.cc
         qt-ui/qt-app.cc
-        qt-ui/qt-utils.cc
-        qt-ui/qt-dialogbox.cc
-        qt-ui/qt-status.cc
-        qt-ui/qt-config_sel.c
-        qt-ui/qt-config.c
         qt-ui/qt-common.c
-        qt-ui/qt-deviceconfig.cc
-        qt-ui/qt-joystickconfig.cc
-        qt-ui/qt-shaderconfig.cc
-        qt-ui/qt-createdisc.cc
+        qt-ui/qt-config.c
+        qt-ui/qt-config_sel.c
         qt-ui/qt-config-eventbinder.cc
+        qt-ui/qt-createdisc.cc
+        qt-ui/qt-deviceconfig.cc
+        qt-ui/qt-dialogbox.cc
+        qt-ui/qt-glslp-parser.c
+        qt-ui/qt-joystickconfig.cc
+        qt-ui/qt-main.cc
         qt-ui/qt-sdl2.c
         qt-ui/qt-sdl2-joystick.c
-        qt-ui/qt-sdl2-mouse.c
         qt-ui/qt-sdl2-keyboard.c
-        qt-ui/qt-sdl2-video.c
+        qt-ui/qt-sdl2-mouse.c
         qt-ui/qt-sdl2-status.c
-        qt-ui/qt-thread.c
-        qt-ui/qt-sdl2-video-renderer.c
+        qt-ui/qt-sdl2-video.c
         qt-ui/qt-sdl2-video-gl3.c
-        qt-ui/qt-glslp-parser.c
+        qt-ui/qt-sdl2-video-renderer.c
         qt-ui/qt-shader_man.c
+        qt-ui/qt-shaderconfig.cc
+        qt-ui/qt-status.cc
+        qt-ui/qt-thread.c
+        qt-ui/qt-utils.cc
+		qt-ui/AboutDlg.ui
+		qt-ui/ConfigureDlg.ui
+		qt-ui/ConfigureSelectionDlg.ui
+		qt-ui/ConfirmRememberDlg.ui
+		qt-ui/CustomResolutionDlg.ui
+		qt-ui/HdNewDlg.ui
+		qt-ui/HdSizeDlg.ui
+		qt-ui/HostConfig.ui
+		qt-ui/ShaderManagerDlg.ui
         )
 
 if(USE_NETWORKING)
